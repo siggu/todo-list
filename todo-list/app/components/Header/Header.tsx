231c23b2f05cf48d1ed348bb855593cfca917369
siggu <m0_w_0m@naver.com>
2025-01-06 23:36:09 +0900

    fix: Header 페이지 이동 오류 해결
    Header에 Link 추가

--- todo-list/app/components/Header/Header.tsx
+++ todo-list/app/components/Header/Header.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
 import styles from './Header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className={`${styles.header}`}>
-      {/* Small Screen */}
-      <div className={styles.smallScreen}>
-        <Image src='/svgs/img/small.svg' alt='Small Header' width={50} height={50} />
-      </div>
-      {/* Medium Screen */}
-      <div className={styles.mediumScreen}>
-        <Image src='/svgs/img/large.svg' alt='Medium Header' width={105} height={50} />
-      </div>
-      {/* Large Screen */}
-      <div className={styles.largeScreen}>
-        <Image src='/svgs/img/large.svg' alt='Large Header' width={105} height={50} />
-      </div>
-    </header>
+    <Link href={'/'}>
+      <header className={`${styles.header}`}>
+        {/* Small Screen */}
+        <div className={styles.smallScreen}>
+          <Image src='/svgs/img/small.svg' alt='Small Header' width={50} height={50} />
+        </div>
+        {/* Medium Screen */}
+        <div className={styles.mediumScreen}>
+          <Image src='/svgs/img/large.svg' alt='Medium Header' width={105} height={50} />
+        </div>
+        {/* Large Screen */}
+        <div className={styles.largeScreen}>
+          <Image src='/svgs/img/large.svg' alt='Large Header' width={105} height={50} />
+        </div>
+      </header>
+    </Link>
   );
 };
 
