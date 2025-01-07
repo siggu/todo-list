9fe0d1ef7812a90a30cf14fa4ca440712d1b4fdb
siggu <m0_w_0m@naver.com>
2025-01-07 19:15:23 +0900

    chore: env 설정 / axios, react-query 설치

--- todo-list/next.config.ts
+++ todo-list/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  env: {
+    TENANT_ID: process.env.TENANT_ID,
+  },
 };
 
 export default nextConfig;
